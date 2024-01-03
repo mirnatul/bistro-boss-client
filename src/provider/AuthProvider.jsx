4bdff46c6abb9bf86d31cb29d9acc6380e7fd7ba
mirnatul <www.mirnatul@gmail.com>
2024-01-04 01:08:31 +0600

    78-8 clear

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -51,17 +51,18 @@ const AuthProvider = ({ children }) => {
             setUser(currentUser);
 
             // get and set token
+
             if (currentUser) {
                 axios.post('http://localhost:5000/jwt', { email: currentUser.email })
                     .then(data => {
                         console.log(data.data.token);
                         localStorage.setItem('access-token', data.data.token);
-                        setLoading(false);
                     })
             }
             else {
                 localStorage.removeItem('access-token');
             }
+            setLoading(false);
         })
         return () => {
             return unsubscribe();
