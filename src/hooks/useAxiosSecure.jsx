b714e1a79ec5c65a83f74a7af6de9404b1176331
mirnatul <www.mirnatul@gmail.com>
2024-01-04 02:21:09 +0600

    huge update axios Interceptor worked perfectly

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
+
+const useAxiosSecure = () => {
+    const { logOut } = useAuth();
+    const navigate = useNavigate();
+
+    // base url
+    const axiosSecure = axios.create({
+        baseURL: 'http://localhost:5000'
+    })
+
+    // interceptor
+    useEffect(() => {
+
+        // request
+        axiosSecure.interceptors.request.use((config) => {
+            const token = localStorage.getItem('access-token');
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`
+            }
+            return config;
+        });
+
+        // response
+        axiosSecure.interceptors.response.use(
+            (response) => response, async (error) => {
+                if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+                    await logOut();
+                    navigate('/login');
+                }
+                return Promise.reject(error);
+            }
+        );
+    }, [logOut, navigate, axiosSecure]);
+
+    return [axiosSecure];
+};
+
+export default useAxiosSecure;
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,24 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
-import { AuthContext } from "../provider/AuthProvider";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
 
 
 
 const useCart = () => {
-    const { user, loading } = useContext(AuthContext);
-    const token = localStorage.getItem('access-token')
-    console.log(token);
+    const { user, loading } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const token = localStorage.getItem('access-token');
+    // console.log(token);
 
     const { isLoading, refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
-        // enabled: !loading,
+        enabled: !loading,
+
+        // queryFn: async () => {
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user.email}`, {
+        //         headers: {
+        //             authorization: `Bearer ${token}`
+        //         }
+        //     })
+        //     return res.json();
+        // }
+
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user.email}`, {
-                headers: {
-                    authorization: `Bearer ${token}`
-                }
-            })
-            return res.json();
+            const res = await axiosSecure(`/carts?email=${user?.email}`);
+            return res.data;
         }
     })
 
